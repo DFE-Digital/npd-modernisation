4f24dc0d5b82707052ede277c6daf6ad54f0364a
Pete Lonsdale <pete@nimbleapproach.com>
2018-10-31 09:44:22 +0000

    amends to funnel prototype

--- app/routes.js
+++ app/routes.js
@@ -3,12 +3,18 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.post('/funnel/dependant-variables', function (req, res) {
+router.post('/funnel/step-4', function (req, res) {
 
-  if (req.session.data['variables1'].includes('Risk')) {
-    res.redirect('/funnel/bespoke/dependant-variables')
+  if (req.session.data['variables3'].includes('Absence Exclusion'))  {
+    res.redirect('/funnel/mixed/step-4')
+  } else if (req.session.data['variables3'].includes('CIN'))  {
+    res.redirect('/funnel/high/step-4')
+  } else if (req.session.data['variables3'].includes('Special category pupils'))  {
+    res.redirect('/funnel/significant/step-4')
+  } else if (req.session.data['variables3'].includes('Personal'))  {
+    res.redirect('/funnel/bespoke/step-4')
   } else {
-    res.redirect('/funnel/dependant-variables')
+    res.redirect('/funnel/step-4')
   }
 })
 
