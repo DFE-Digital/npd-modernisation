154a4eeb5235b2afb378891851736bb8cdfc8d3b
Pete Lonsdale <pete@nimbleapproach.com>
2019-09-02 15:27:51 +0100

    first draft of accessibility content

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Find NPD Data',
+  serviceName: 'Find and Explore data in the National Pupil Database',
 
   // Default port that prototype runs on
   port: '3000',
