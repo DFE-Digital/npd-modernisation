d9c8daa94bc011e01e78cf53a622f626530b02f6
Pete Lonsdale <pete@nimbleapproach.com>
2019-11-20 15:12:26 +0000

    added future version of the dataset view home and detail page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -106,7 +106,7 @@ function copyToClipboard(element) {
   document.execCommand("copy");
   $temp.remove();
 
-  $('<div class="npd-copy-success govuk-!-margin-bottom-1 govuk-!-margin-top-2"><h2 class="govuk-heading-s">Items copied</h2><p class="govuk-!-font-size-16">Those data items are in your clipboard, head to your application and paste them into the Data section.</p> </div>')
+  $('<div class="npd-copy-success govuk-!-margin-bottom-1 govuk-!-margin-top-2"><h2 class="govuk-heading-s">Items copied</h2><p class="govuk-!-font-size-16">Those data items are in your clipboard, you can now paste them into another document.</p> </div>')
   .insertAfter('#copyButton')
   .fadeIn()
   .delay(5000)
